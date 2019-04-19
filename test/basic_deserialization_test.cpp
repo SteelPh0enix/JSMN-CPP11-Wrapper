1e602a9168a600cc80b135f4e3b244dfcd31a8c2
Wojciech Olech <steelphoenix2137@gmail.com>
2019-04-19 15:39:07 +0200

    Added more test cases and support to char/unsigned char casting (as numbers, not string characters)

--- test/basic_deserialization_test.cpp
+++ test/basic_deserialization_test.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+#include <cstring>
 #include <jsmn.hpp>
 
 TEST_CASE("Deserialize simple, flat JSON with one int value",
@@ -16,8 +17,8 @@ TEST_CASE("Deserialize JSON with several integer values", "[deserialization]") {
 
   REQUIRE(j.parse() == 7);
   REQUIRE(j.get<int>("first value") == 123);
-  REQUIRE(j.get<int>("second value") == 456);
-  REQUIRE(j.get<int>("third value") == 789);
+  REQUIRE(j.get<long>("second value") == 456);
+  REQUIRE(j.get<unsigned long long>("third value") == 789);
 }
 
 TEST_CASE("Deserialize JSON with floating-point values", "[deserialization]") {
@@ -35,4 +36,57 @@ TEST_CASE("Deserialize JSON with boolean values", "[deserialization]") {
   REQUIRE(j.parse() == 5);
   REQUIRE(j.get<bool>("this is true") == true);
   REQUIRE(j.get<bool>("this is false") == false);
+}
+
+TEST_CASE("Check if JsonParser returns default value when no key is found",
+          "[deserialization]") {
+  JsonParser<1> j("{}");
+
+  REQUIRE(j.parse() == 1);
+  REQUIRE(j.get<bool>("test") == false);
+  REQUIRE(j.get<int>("test") == 0);
+  REQUIRE(j.get<unsigned>("test") == 0u);
+  REQUIRE(j.get<float>("test") == 0.f);
+  REQUIRE(j.get<double>("test") == 0.);
+}
+
+TEST_CASE("No-copy string parsing test", "[deserialization]") {
+  JsonParser<5> j(
+      R"({"TestString1": "This is a test", "teststr": "another test string"})");
+
+  REQUIRE(j.parse() == 5);
+
+  auto testString1View = j.get<JsonStringView>("TestString1");
+  auto teststrView = j.get<JsonStringView>("teststr");
+  auto invalidView = j.get<JsonStringView>("does not exists");
+
+  REQUIRE(testString1View.length == strlen("This is a test"));
+  REQUIRE(strncmp(testString1View.str, "This is a test",
+                  testString1View.length) == 0);
+
+  REQUIRE(teststrView.length == strlen("another test string"));
+  REQUIRE(strncmp(teststrView.str, "another test string", teststrView.length) ==
+          0);
+
+  REQUIRE(invalidView.length == 0);
+  REQUIRE(invalidView.str == nullptr);
+}
+
+TEST_CASE("String parsing test, with copying", "[deserialization]") {
+  JsonParser<5> j(
+      R"({"TestString1": "This is a test", "teststr": "another test string"})");
+
+  REQUIRE(j.parse() == 5);
+
+  char testString1Buf[32];
+  char teststrBuf[32];
+  char notExistingBuf[32];
+
+  REQUIRE(j.getString("TestString1", testString1Buf) == true);
+  REQUIRE(j.getString("teststr", teststrBuf) == true);
+  REQUIRE(j.getString("does not exists", notExistingBuf) == false);
+  REQUIRE(j.getString("teststr", nullptr) == false);
+
+  REQUIRE(strcmp(testString1Buf, "This is a test") == 0);
+  REQUIRE(strcmp(teststrBuf, "another test string") == 0);
 }
